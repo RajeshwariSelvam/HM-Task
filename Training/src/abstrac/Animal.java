4e84cac8b139aae6bf602ff6d3e4110f2b8bfa4e
Arun Ganesan <Arun Ganesan@192.168.11.67>
2020-02-20 15:25:35 +0530

    20-02-19

--- Training/src/abstrac/Animal.java
+++ Training/src/abstrac/Animal.java
@@ -5,9 +5,7 @@ public abstract class Animal {
   static int a =100 ;
 	abstract void sound();
 	
-    abstract int sound(String s);
-	abstract String sound(int a);
-	
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
