6827fb6379b3c13efd6bffea3b567368d72f4606
Arun Ganesan <Arun Ganesan@192.168.11.67>
2020-02-20 15:29:02 +0530

    third time

--- Training/src/abstrac/Animal.java
+++ Training/src/abstrac/Animal.java
@@ -2,16 +2,21 @@ package abstrac;
 
 public abstract class Animal {
 
-  static int a =100 ;
+	static int a = 100;
+
 	abstract void sound();
-	
-    
+
+	static void test() {
+	}
+
+	static void test1() {
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("String Class");
-		
-		
+
 	}
 
 }
